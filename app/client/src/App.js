7b5b9025b0a903dfb8fca7a641208165331e35ab
pure-coder <jrd259@my.open.ac.uk>
2018-08-17 20:13:27 +0100

    navbar design

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Navigation from './components/layout/Navigator';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation/>
         <h1>Welcome page!</h1>
       </div>
     );
