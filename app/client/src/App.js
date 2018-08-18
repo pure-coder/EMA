6442bd9a89dccc8b02b30d7d20e4e5da28e7c935
pure-coder <jrd259@my.open.ac.uk>
2018-08-18 16:14:43 +0100

    Added BrowserRouter for routes

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import Navigation from './components/layout/Navigator';
-import Landing_page from './components/layout/Landing_page';
-import Footer from './components/layout/Footer';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'; /* mimics server allowing back, forward page routing etc*/
+
+import Navigation from './components/layout/Navigator'; /*import navbar component*/
+import Landing_page from './components/layout/Landing_page'; /*import landing_page component*/
+import Footer from './components/layout/Footer'; /*import footer component*/
+
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" id="wrap">
-        <Navigation/>
-        <Landing_page id="main"/>
-          <Footer/>
-      </div>
+        <Router>
+            {/* Wrap the App div with Router so the functionality can be used  */}
+            <div className="App" id="wrap">
+                <Navigation/> {/*adds navbar component to web app*/}
+                <Route exact path="/" component={Landing_page} /> {/* using Route functionality that adds the landing_page component to web app*/}
+                <Footer/> {/*adds footer component to web app*/}
+            </div>
+        </Router>
     );
   }
 }
