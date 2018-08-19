ef53bd478f6ff80f0fa4ee0763b9fa29839928f4
pure-coder <jrd259@my.open.ac.uk>
2018-08-19 18:15:54 +0100

    added compose from redux in store.js

--- app/client/src/store.js
+++ app/client/src/store.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -8,7 +8,10 @@ const initialisedState = {}; // Allows a preloaded state to be passed into the s
 const middleware = [thunk];
 
 // Used to create store to hold application state, 1st parameter of createStore is the root reducer (specifies how app state
-// changes in response to actions), 2nd parameter takes the initialisedState, 3rd parameter takes the thunk middleware.
-const store = createStore(rootReducer, initialisedState, applyMiddleware(...middleware));
+// changes in response to actions), 2nd parameter takes the initialisedState, 3rd parameter takes the thunk middleware (compose
+// allows actions to be passed as parameters instead of nesting them).
+const store = createStore(rootReducer, initialisedState, compose(applyMiddleware(...middleware),
+
+    ));
 
 export default  store;
