10a34d3eb3f32f47ffc4a2507790cb9e62847690
pure-coder <jrd259@my.open.ac.uk>
2018-08-22 17:03:42 +0100

    Set up setAuthorisationToken for login

--- app/client/src/actions/authenticationActions.js
+++ app/client/src/actions/authenticationActions.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import {GET_ERRS} from "./types";
+import {GET_ERRS} from "./types"; // import custom defined types
+import setAuthorisationToken from '../utilities/setAuthorisationToken';
 
 // Register User
 // Used to dispatch (action) data to a reducer, in this case it is the registerUser action with the sign up data
 // from the redux state entered in the register route and handled by Register.js in components/authentication folder.
-
 // Note that dispatch is an asynchronous function - redux thunk allows asynchronous functions to be used as actions.
 // history is passed in from register.js onSubmit so it can be used to direct the user to another link/route
 export const registerUser =(Data, history) => (dispatch) => {
@@ -19,4 +19,30 @@ export const registerUser =(Data, history) => (dispatch) => {
             payload: err.response.data // Puts err data into the payload which will be sent to the redux store
         })
     );
+}
+
+// Login User - get JWT for user
+
+export const loginUser =(Data, history) => (dispatch) => {
+    // Post user data to the API specifically the user/register route
+    axios
+        .post('/api/login', Data)
+        .then(result => {
+            // Save JWT to local storage
+            const { token } = res.data;
+            // Set the token to local storage item 'jwtToken' (local storage can only store strings!)
+            localStorage.setItem('jwtToken', token);
+            // Set the token to authorisation header
+            setAuthorisationToken(token);
+
+            // Set
+
+        })
+        .catch(err =>
+            dispatch({ // if an error occurs dispatch is called to send the data as an object to the
+                // redux store, in this case the err data
+                type: GET_ERRS,
+                payload: err.response.data // Puts err data into the payload which will be sent to the redux store
+            })
+        );
 }
---
+++ app/client/src/utilities/setAuthorisationToken.js
@@ -0,0 +1,15 @@
+import axios form 'axios';
+
+const setAuthorisationToken = (token) => {
+    // Check if the token exists
+    if(token){
+        // Apply token to every request (main reason for using axios over fetch)
+        axios.defaults.headers.common['Authorization'] = token;
+    }
+    // if token doesn't exist then delete the authorisation header
+    else {
+        delete axios.defaults.headers.common['Authorization'];
+    }
+};
+
+export default setAuthorisationToken;
