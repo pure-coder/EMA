2699563a24065251e4282e3dec4c0a3d14eedcb0
pure-coder <jrd259@my.open.ac.uk>
2019-02-14 08:03:37 +0000

    Updated personalTrainer schema to use refs, also added client schema to file

--- app/models/PersonalTrainer.js
+++ app/models/PersonalTrainer.js
@@ -1,6 +1,43 @@
 const mongoose = require('mongoose');
 
-// Create Schema
+// Create Client Schema
+const ClientSchema = mongoose.Schema({
+    FullName: {
+        // Trim
+        type: String, index: true, trim: true
+    },
+    Email: {
+        // Trim and lowercase
+        type: String, index: {unique: true}, lowercase: true, trim: true,
+    },
+    DateOfBirth: {
+        type: Date, trim: true,
+    },
+    Password: {
+        // default password is a hash of "password" - will need to be changed by user on initial login
+        type: String, default: "$2a$12$gyS3ECpQsn0RZivqKG8HK.Vv5kBtcfilMI.IlzK2xas6PcxBeEZ12", trim: true,
+    },
+    ContactNumber: {
+        type: String, trim: true,
+    },
+    Sex: {
+        type: String, default: 'NA', trim: true,
+    },
+    ProfilePicUrl: {
+        type: String, default: 'NA', trim: true,
+    },
+    Activated: {
+        type: Boolean, default: false
+    },
+    Date: {
+        type: Date
+    },
+    ptId: {
+        type: String
+    }
+});
+
+// Create Personal Trainer Schema
 const PersonalTrainerSchema = mongoose.Schema({
     FullName: {
         // Trim
@@ -29,14 +66,11 @@ const PersonalTrainerSchema = mongoose.Schema({
         type: Date
     }
     ,
-    ClientIDs: [{
-        FullName: {type: String},
-        email: {type: String},
-        id: {type: String},
-        ptId: {type: String}
-    }]
+    ClientIDs: [
+        {type: Schema.Types.ObjectId, ref: 'clients', unique: true}
+        ]
     // collection value is the name of the collection that is stored in the database
 }, {collection: "Personal_Trainers"}, {timestamps: true});
 
-
+const Clients = mongoose.model('clients', ClientSchema);
 module.exports = mongoose.model('PersonalTrainer', PersonalTrainerSchema);
