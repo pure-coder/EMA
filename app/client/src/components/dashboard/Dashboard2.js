d9fc87452f6185e0e2d8a01d7559d4b34d13b5a2
pure-coder <jrd259@my.open.ac.uk>
2019-03-06 15:02:40 +0000

    Dashboard2 moved addGraph function to external file

--- app/client/src/components/dashboard/Dashboard2.js
+++ app/client/src/components/dashboard/Dashboard2.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'; // Used to document prop types sent to compo
 import { connect } from 'react-redux' // Needed when using redux inside a component (connects redux to this component)
 import { withRouter } from 'react-router-dom';
 import {getClientProgression} from "../../actions/authenticationActions";
+import {addGraph} from '../../utilities/progressGraph'
 import * as d3 from 'd3';
 // import FormInputGroup from "../common/FormInputGroup"; // Allows proper routing and linking using browsers match, location, and history properties
 
@@ -33,128 +34,7 @@ class Dashboard2 extends Component {
         }
     } // did mount
 
-    addGraph(data, position, title) {// set the dimensions and margins of the graph
 
-        // Takes data given in function
-        let dataToDraw = data;
-
-        let marginTop = 20;
-        let marginRight = 20;
-        let marginBottom = 30;
-        let marginLeft = 50;
-        let width = 500 - marginLeft - marginRight;
-        let height = 310 - marginTop - marginBottom;
-
-        // parse the {Date / time
-        let parseTime = d3.timeParse("%Y-%d-%mT%H:%M:%S.%L%Z");
-
-        // set the ranges
-        let x = d3.scaleTime().range([0, width]);
-        let y = d3.scaleLinear().range([height, 0]);
-
-        // define the area to shade (fill)
-        let area = d3.area()
-            .x(function(d) { return x(d.Date); })
-            .y0(height)
-            .y1(function(d) { return y(d.maxWeight); });
-
-        // define the line
-        let valueline = d3.line()
-            .x(function (d) {
-                return x(d.Date);
-            })
-            .y(function (d) {
-                return y(d.maxWeight);
-            });
-
-        // append the svg object to the body of the page
-        // appends a 'group' element to 'svg'
-        // moves the 'group' element to the top left margin
-        // setPosition - sets specific graph to div specified as position parameter
-        let svg = d3.select(position).append("svg")
-            .attr("width", width + marginLeft + marginRight)
-            .attr("height", height + marginTop + marginBottom)
-            .append("g")
-            .attr("transform",
-                "translate(" + marginLeft + "," + marginTop + ")");
-
-        // Get the data
-        function draw(data) {
-
-            // format the data
-            data.forEach(function (d) {
-                console.log(d.Date)
-                d.Date = parseTime(d.Date);
-                console.log(d.Date);
-                d.maxWeight = +d.maxWeight;
-            });
-
-            // Scale the range of the data
-            x.domain(d3.extent(data, function (d) {
-                return d.Date;
-            }));
-            // Changed y-axis to use the min to max range of data like what is used in x-axis
-            y.domain(d3.extent(data, function (d) {
-                return d.maxWeight;
-            }));
-
-            // add the area
-            svg.append("path")
-                .data([data])
-                .attr("class", "area")
-                .attr("d", area);
-
-            // Add the valueline path.
-            svg.append("path")
-                .data([data])
-                .attr("class", "line")
-                .attr("d", valueline);
-
-            // text label for the graph title
-            svg.append("text")
-                .attr("transform",
-                    "translate(" + (width / 2) + " ," + // first value is distance from left, second is distance from top
-                    (-5) + ")")
-                .style("text-anchor", "middle")
-                .text(title);
-
-            // Add the X Axis
-            svg.append("g")
-                .attr("transform", "translate(0," + height + ")")
-                // tickValues used to display only the dates given in the data
-                .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%d %b %Y")))
-                .selectAll("text")
-                .style("text-anchor", "end")
-                .attr("dx", "-.8em")
-                .attr("dy", ".15em")
-                .attr("transform", "rotate(-65)");
-
-            // text label for the x axis
-            svg.append("text")
-                .attr("transform",
-                    "translate(" + (width / 2) + " ," +
-                    (height + marginTop + 62) + ")")
-                .style("text-anchor", "middle")
-                .text("Date");
-
-            // Add the Y Axis
-            svg.append("g")
-                .call(d3.axisLeft(y));
-
-            // text label for the y axis
-            svg.append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 0 - marginLeft)
-                .attr("x", 0 - (height / 2))
-                .attr("dy", "1em")
-                .style("text-anchor", "middle")
-                .text("Weight (Kg)"); // y-axis label
-
-        } // draw
-
-        draw(dataToDraw);
-
-    } // addGraph
 
     render() {
         //const {errors} = this.state; // This allows errors to be pulled out of this.state with pulling them out directly
@@ -170,7 +50,7 @@ class Dashboard2 extends Component {
                     return progressData.push(data);
                 });
                 // 1st argument takes array of objects as data to plot graph, 2nd argument takes div as position to display graph, 3rd is title of graph
-                this.addGraph(progressData, ".progression-data", element.exerciseName);
+                addGraph(progressData, ".progression-data", element.exerciseName);
                 return null;
             });
         } // if client_progression is not undefined
