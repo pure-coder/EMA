58744e92c5289f453ea1a202055c80a5dd3e1796
pure-coder <jrd259@my.open.ac.uk>
2019-03-06 11:35:21 +0000

    Dashboard2 client progression showing on chart, a few bugs to sort out -- like date increment, kg increment, showing title of exercise

--- app/client/src/components/dashboard/Dashboard2.js
+++ app/client/src/components/dashboard/Dashboard2.js
@@ -11,14 +11,18 @@ class Dashboard2 extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            client_Progression: null,
             errors: {}
         };
 
-        // // This sets the state value to it's respective state (via binding)
-        // this.onChange = this.onChange.bind(this);
-        //
-        // // This binds the onSubmit function to this.OnSubmit
-        // this.onSubmit = this.onSubmit.bind(this);
+        this.props.getClientProgression(this.props.authenticatedUser.user.id, '5c2e2e604489901a743d87db');
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        if (props.authenticatedUser.client_Progression !== state.client_Progression) {
+            return {client_Progression: props.authenticatedUser.client_Progression};
+        }
+        return null
     }
 
     // Life cycle method for react which will run when this component receives new properties
@@ -27,143 +31,138 @@ class Dashboard2 extends Component {
         if (!this.props.authenticatedUser.isAuthenticated) {
             this.props.history.push('/login');
         }
+    } // did mount
 
-        this.props.getClientProgression('5c2e2d484489901a743d87da', '5c2e2e604489901a743d87db');
+    addGraph(data, position) {// set the dimensions and margins of the graph
 
+        // Takes data given in function
+        let dataToDraw = data;
 
-        let myData = [
-            {date: '2019-01-03T15:42:00.238+0000', frequency: 1.13},
-            {date: '2019-02-03T15:42:00.238+0000', frequency: 5.00},
-            {date: '2019-03-03T15:42:00.238+0000', frequency: 10},
-            {date: '2019-04-03T15:42:00.238+0000', frequency: 20},
-            {date: '2019-05-03T15:42:00.238+0000', frequency: 30},
-            {date: '2019-06-03T15:42:00.238+0000', frequency: 35},
-            {date: '2019-07-03T15:42:00.238+0000', frequency: 42},
-            {date: '2019-08-03T15:42:00.238+0000', frequency: 55},
-            {date: '2019-09-03T15:42:00.238+0000', frequency: 55},
-            {date: '2019-10-03T15:42:00.238+0000', frequency: 60},
-            {date: '2019-12-03T15:42:00.238+0000', frequency: 66},
-            {date: '2020-01-03T15:42:00.238+0000', frequency: 68},
-            {date: '2020-02-03T15:42:00.238+0000', frequency: 62},
-            {date: '2020-04-03T15:42:00.238+0000', frequency: 70},
-            {date: '2020-05-03T15:42:00.238+0000', frequency: 76}
-        ];
+        let marginTop = 20;
+        let marginRight = 20;
+        let marginBottom = 30;
+        let marginLeft = 50;
+        let width = 500 - marginLeft - marginRight;
+        let height = 310 - marginTop - marginBottom;
 
-        // 1st parameter takes array of objects as data to plot graph, 2nd parameter takes div as position to display graph
-        function addGraph(data, position) {// set the dimensions and margins of the graph
+        // parse the {Date / time
+        let parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S.%L%Z");
 
-            // Takes data given in function
-            let myData = data;
+        // set the ranges
+        let x = d3.scaleTime().range([0, width]);
+        let y = d3.scaleLinear().range([height, 0]);
 
-            let marginTop = 20;
-            let marginRight = 20;
-            let marginBottom = 30;
-            let marginLeft = 50;
-            let width = 500 - marginLeft - marginRight;
-            let height = 310 - marginTop - marginBottom;
+        // define the line
+        let valueline = d3.line()
+            .x(function (d) {
+                return x(d.Date);
+            })
+            .y(function (d) {
+                return y(d.maxWeight);
+            });
 
-            // parse the {date / time
-            let parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S.%L%Z");
+        // append the svg object to the body of the page
+        // appends a 'group' element to 'svg'
+        // moves the 'group' element to the top left margin
+        // setPosition - sets specific graph to div specified as position parameter
+        let svg = d3.select(position).append("svg")
+            .attr("width", width + marginLeft + marginRight)
+            .attr("height", height + marginTop + marginBottom)
+            .append("g")
+            .attr("transform",
+                "translate(" + marginLeft + "," + marginTop + ")");
 
-            // set the ranges
-            let x = d3.scaleTime().range([0, width]);
-            let y = d3.scaleLinear().range([height, 0]);
+        // Get the data
+        function draw(data) {
 
-            // define the line
-            let valueline = d3.line()
-                .x(function (d) {
-                    return x(d.date);
-                })
-                .y(function (d) {
-                    return y(d.frequency);
-                });
+            // format the data
+            data.forEach(function (d) {
+                d.Date = parseTime(d.Date);
+                d.maxWeight = +d.maxWeight;
+            });
 
-            // append the svg object to the body of the page
-            // appends a 'group' element to 'svg'
-            // moves the 'group' element to the top left margin
-            // setPosition - sets specific graph to div specified as position parameter
-            let svg = d3.select(position).append("svg")
-                .attr("width", width + marginLeft + marginRight)
-                .attr("height", height + marginTop + marginBottom)
-                .append("g")
+            // Scale the range of the data
+            x.domain(d3.extent(data, function (d) {
+                return d.Date;
+            }));
+            y.domain([0, d3.max(data, function (d) {
+                return d.maxWeight;
+            })]);
+
+            // Add the valueline path.
+            svg.append("path")
+                .data([data])
+                .attr("class", "line")
+                .attr("d", valueline);
+
+            // text label for the graph title
+            svg.append("text")
                 .attr("transform",
-                    "translate(" + marginLeft + "," + marginTop + ")");
+                    "translate(" + (width / 2) + " ," + // first value is distance from left, second is distance from top
+                    (-5) + ")")
+                .style("text-anchor", "middle")
+                .text("Title");
 
-            // Get the data
-            function draw(data) {
+            // Add the X Axis
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%m %b %Y")))
+                .selectAll("text")
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", ".15em")
+                .attr("transform", "rotate(-65)");
 
-                // format the data
-                data.forEach(function (d) {
-                    d.date = parseTime(d.date);
-                    d.frequency = +d.frequency;
-                });
+            // text label for the x axis
+            svg.append("text")
+                .attr("transform",
+                    "translate(" + (width / 2) + " ," +
+                    (height + marginTop + 62) + ")")
+                .style("text-anchor", "middle")
+                .text("Date");
 
-                // Scale the range of the data
-                x.domain(d3.extent(data, function (d) {
-                    return d.date;
-                }));
-                y.domain([0, d3.max(data, function (d) {
-                    return d.frequency;
-                })]);
+            // Add the Y Axis
+            svg.append("g")
+                .call(d3.axisLeft(y));
 
-                // Add the valueline path.
-                svg.append("path")
-                    .data([data])
-                    .attr("class", "line")
-                    .attr("d", valueline);
+            // text label for the y axis
+            svg.append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 0 - marginLeft)
+                .attr("x", 0 - (height / 2))
+                .attr("dy", "1em")
+                .style("text-anchor", "middle")
+                .text("Weight (Kg)"); // y-axis label
 
-                // text label for the graph title
-                svg.append("text")
-                    .attr("transform",
-                        "translate(" + (width/2) + " ," + // first value is distance from left, second is distance from top
-                        (-5) + ")")
-                    .style("text-anchor", "middle")
-                    .text("Title");
+        } // draw
 
-                // Add the X Axis
-                svg.append("g")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%m %b %Y")))
-                    .selectAll("text")
-                    .style("text-anchor", "end")
-                    .attr("dx", "-.8em")
-                    .attr("dy", ".15em")
-                    .attr("transform", "rotate(-65)" );
+        draw(dataToDraw);
 
-                // text label for the x axis
-                svg.append("text")
-                    .attr("transform",
-                        "translate(" + (width/2) + " ," +
-                        (height + marginTop + 62) + ")")
-                    .style("text-anchor", "middle")
-                    .text("Date");
-
-                // Add the Y Axis
-                svg.append("g")
-                    .call(d3.axisLeft(y));
-
-                // text label for the y axis
-                svg.append("text")
-                    .attr("transform", "rotate(-90)")
-                    .attr("y", 0 - marginLeft)
-                    .attr("x",0 - (height / 2))
-                    .attr("dy", "1em")
-                    .style("text-anchor", "middle")
-                    .text("Weight (Kg)"); // y-axis label
-
-            } // d3.json
-
-            draw(myData);
-        } // addGraph
-
-        // 1st argument takes array of objects as data to plot graph, 2nd argument takes div as position to display graph
-        addGraph(myData, ".progression-data");
-        // addGraph(myData2, ".progression-data2");
-
-    }
+    } // addGraph
 
     render() {
         //const {errors} = this.state; // This allows errors to be pulled out of this.state with pulling them out directly
+
+        // 1st argument takes array of objects as data to plot graph, 2nd argument takes div as position to display graph
+        // addGraph(myData, ".progression-data");
+        // addGraph(myData2, ".progression-data2");
+
+        let client_progression = this.state.client_Progression;
+        // console.log(client_progression !== undefined ? client_progression[0] : null);
+
+        // Do from dashboard on client click so this check is not needed as it would be populated
+        if (client_progression !== undefined){
+            client_progression.map(element => {
+                let progressData = [];
+                element.metrics.map(data =>{
+                    return progressData.push(data);
+                });
+                this.addGraph(progressData, ".progression-data");
+                return null;
+            });
+
+            console.log(client_progression);
+        } // if client_progression is not undefined
 
         return (
             <div className="container  dashboard-custom">
