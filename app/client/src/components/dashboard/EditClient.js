10f78b553ab59f916ee727378ef79bb70747643d
pure-coder <jrd259@my.open.ac.uk>
2019-03-30 11:53:24 +0000

    30/03/19

--- app/client/src/components/dashboard/EditClient.js
+++ app/client/src/components/dashboard/EditClient.js
@@ -158,6 +158,10 @@ class EditClient extends Component {
         }
         else {
             let {errors, message} = this.state;
+
+            if(!isEmpty(this.state.client_data.FullName)){
+                console.log(this.state.client_data.FullName);
+            }
             return (
                 <div className="edit_client">
                     <div className="container  edit_client-custom">
