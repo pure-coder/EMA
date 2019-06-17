46da870ad4eb36e9f8ac20cbaa624191a54c4e1e
pure-coder <jrd259@my.open.ac.uk>
2019-06-17 16:54:38 +0100

    Modal css styling, (Height) completed.

--- app/client/src/components/dashboard/clients/DeleteConfirm.js
+++ app/client/src/components/dashboard/clients/DeleteConfirm.js
@@ -16,27 +16,33 @@ class DeleteConfirm extends Component {
     } // constructor
 
     static getDerivedStateFromProps(props, state) {
-        if (props.progressFormHeight < state.progressFormHeight) {
+        if (props.progressFormHeight !== state.progressFormHeight) {
             return {
-                progressFormHeight: props.progressFormHeight
+                progressFormHeight: state.progressFormHeight
             }
         }
         return null;
     }
 
-    componentDidMount(){
+    // Get height of Modal
+    getModalHeight(){
         let formHeight;
         let el = document.querySelector(".delete-confirm");
         formHeight = el.offsetHeight;
         formHeight += parseInt(window.getComputedStyle(el).getPropertyValue('margin-top'), 10);
         formHeight += parseInt(window.getComputedStyle(el).getPropertyValue('margin-bottom'), 10);
-        this.setState({progressFormHeight: formHeight});
+        return formHeight;
+    }
+
+    componentDidMount(){
     }
 
     componentDidUpdate(prevProps){
-        if(this.state.progressFormHeight < prevProps.progressFormHeight ){
-            let newHeight = this.state.progressFormHeight;
-            this.props.modalSize(newHeight.toString())
+        // Get's height of Modal after the page has updated (changed) and compares it to previous state, if state
+        // is not equal then update size of Modal
+        let formHeight = this.getModalHeight();
+        if(formHeight !== prevProps.progressFormHeight ){
+            this.props.modalSize(formHeight.toString())
         }
     }
 
@@ -53,10 +59,12 @@ class DeleteConfirm extends Component {
         return (
             <div className="delete-confirm">
                 <h2 className="mt-3 mb-3"> Are you sure you want to delete data for {this.props.name}? </h2>
-                <input id="confirm" type="button" className="btn btn-success btn-block mb-4"
-                       value="Confirm" onClick={this.onConfirm} />
-                <input id="cancel" type="button" className="btn btn-danger btn-block mb-4"
-                       value="Cancel" onClick={this.onCancel} />
+                <div className="confirm-buttons">
+                    <input id="confirm" type="button" className="btn btn-success btn-block mb-4"
+                           value="Confirm" onClick={this.onConfirm} />
+                    <input id="cancel" type="button" className="btn btn-danger btn-block mb-4"
+                           value="Cancel" onClick={this.onCancel} />
+                </div>
             </div>
         );
 
