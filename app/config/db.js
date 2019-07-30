5cbceb969ff90ca4790365b5b8904a4023287657
pure-coder <jrd259@my.open.ac.uk>
2019-07-30 16:40:35 +0100

    Adding mongodb atlas.

--- app/config/db.js
+++ app/config/db.js
@@ -4,3 +4,9 @@ module.exports = {
         dsn: 'mongodb://192.168.99.100:37017/fitness_app',
     }
 };
+
+//mongodb atlas
+//mongodb+srv://jrdunkley:<password>@jrdcluster-c4okw.mongodb.net/test?retryWrites=true&w=majority
+
+// local_db
+//dsn: 'mongodb://192.168.99.100:37017/fitness_app',
