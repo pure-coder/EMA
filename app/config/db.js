2cc02d32a9a57547f09d4690dbae13ba62fc8280
pure-coder <jrd259@my.open.ac.uk>
2019-07-30 16:50:13 +0100

    password added to mongodb atlas.

---
+++ app/config/db.js
@@ -0,0 +1,12 @@
+module.exports = {
+    secretOrKey: 'FitnessAppProjectSecretKey',
+    mongodb: {
+        dsn: 'mongodb://192.168.99.100:37017/fitness_app',
+    }
+};
+
+// mongo atlas
+// mongodb+srv://jrdunkley:gokuhson4A@jrdcluster-c4okw.mongodb.net/test?retryWrites=true&w=majority
+
+// local
+// dsn: 'mongodb://192.168.99.100:37017/fitness_app',
