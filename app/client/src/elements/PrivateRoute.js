357552cea9258bb00203396508a7c4e944e1b644
pure-coder <jrd259@my.open.ac.uk>
2019-08-12 17:47:08 +0100

    Token refreshed everytime user visits dashboard, working correctly.

--- app/client/src/elements/PrivateRoute.js
+++ app/client/src/elements/PrivateRoute.js
@@ -1,34 +1,27 @@
 // Used to redirect user if they are not authenticated to view a page
 import store from "../store";
 import {Redirect, Route} from "react-router-dom";
-import isEmpty from "../utilities/is_empty";
 import React from "react";
 import {checkExp} from "../utilities/checkExp";
 
 const PrivateRoute = ({component: Component, ...rest}) => {
 
-    let auth = store.getState();
+    const auth = store.getState();
+    const {isAuthenticated} = auth.authenticatedUser;
+
     return(
         <Route
             {...rest}
             render={(props) =>
             {
                 checkExp(auth);
-                if (isEmpty(auth.authenticatedUser)) {
+                if (!isAuthenticated) {
                     // console.log("auth empty - pr")
                     // If direct url used and auth is empty, this will send user to login screen!
                     return <Redirect to={{pathname: '/login', state: {from: props.location}}}/>
                 }
                 else {
-                    if (auth.authenticatedUser.isAuthenticated === true) {
-                        // console.log("auth true - pr")
-                        return <Component {...props} />
-                    }
-                    else {
-                        // console.log("not logged in - pr")
-                        // If direct url used and not logged in, this will send user to login screen!
-                        return <Redirect to={{pathname: '/login', state: {from: props.location}}}/>
-                    }
+                    return <Component {...props} />
                 }
             }
             }
--- app/client/src/utilities/checkExp.js
+++ app/client/src/utilities/checkExp.js
@@ -1,13 +1,14 @@
 import {logOutUser} from "../actions/authenticationActions";
 
 export const checkExp = (store) => {
-    const expiredTime = (store.authenticatedUser.user.exp * 1000);
-    const currentTime = Date.now();
-    if (expiredTime < currentTime) {
-        // Log the user out
-        store.dispatch(logOutUser());
-        // Redirect the user to the login page
-        window.location.href = '/re-login';
-    } // if decodedToken
-
+    if(store.authenticatedUser.isAuthenticated){
+        const expiredTime = (store.authenticatedUser.user.exp * 1000);
+        const currentTime = Date.now();
+        if (expiredTime < currentTime) {
+            // Log the user out
+            store.dispatch(logOutUser());
+            // Redirect the user to the login page
+            window.location.href = '/re-login';
+        } // if decodedToken
+    }
 };
