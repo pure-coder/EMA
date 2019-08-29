4f4397ec2d337e89d9a2f2f47a9e9682671f3f0b
James Dunkley <JRDUNKLEY101@GMAIL.COM>
2019-08-29 21:28:28 +0100

    Delete db.js

--- app/config/db.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    secretOrKey: 'FitnessAppProjectSecretKey',
-    mongodb: {
-        dsn: 'mongodb+srv://jrdunkley:gokuhson4A@jrdcluster-c4okw.mongodb.net/fitness_app',
-    }
-};
