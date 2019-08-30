ed52464add1e9d4711ce8a77fbd7cfd451978aa7
pure-coder <jrd259@my.open.ac.uk>
2019-08-30 02:46:12 +0100

    Checking aws settings

--- app/config/config_data.js
+++ app/config/config_data.js
@@ -2,5 +2,5 @@ if(process.env.NODE_ENV === 'production'){
     module.exports = require('./prod_config');
 }
 else{
-    module.exports = require('./db');
+    module.exports = require('./db')
 }
--- app/config/db.js
+++ app/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
     secretOrKey: 'FitnessAppProjectSecretKey',
     mongodb: {
-        dsn: 'mongodb+srv://jrdunkley:gokuhson4A@jrdcluster-c4okw.mongodb.net/fitness_app',
+        dsn: 'mongodb+srv://jrdunkley:gokuhson4A@jrdcluster-c4okw.mongodb.net/fitness_app'
     }
 };
