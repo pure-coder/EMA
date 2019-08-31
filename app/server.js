f3580505943c8972872c478a33f5bf5ec698a05d
pure-coder <jrd259@my.open.ac.uk>
2019-08-31 05:05:00 +0100

    Set up env variable in .bashrc and sourced.

--- app/server.js
+++ app/server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 
 
 // require database setup in the config folder
-const db = require('./config/db');
+const db = require('./config/prod_config');
 
 // Connect to MongoDB
 
