e8846db25b5efea468d377ad0499434817955fa2
pure-coder <jrd259@my.open.ac.uk>
2019-09-03 00:15:00 +0100

    Sorted RegisterClient redirect, loaded state was not needed, changed loading screen check if isAuthenticated is false, gives time for logout of user then redirect to homepage.

--- app/client/src/components/authentication/RegisterClient.js
+++ app/client/src/components/authentication/RegisterClient.js
@@ -6,9 +6,9 @@ import { withRouter } from 'react-router-dom';
 import FormInputGroup from "../common/Forms/FormInputGroup";
 import Loading from "../common/Loading/Loading";
 import isEmpty from "../../utilities/is_empty";
-import ErrorComponent from "../error/ErrorComponent";
 import FormSelectComp from "../common/Forms/FormSelectComp";
-import DisplayMessage from "../common/Message/DisplayMessage"; // Allows proper routing and linking using browsers match, location, and history properties
+import DisplayMessage from "../common/Message/DisplayMessage";
+import checkExp from "../../utilities/checkExp"; // Allows proper routing and linking using browsers match, location, and history properties
 
 class RegisterClient extends Component {
     // This allows the component states to be updated and re-rendered
@@ -27,16 +27,11 @@ class RegisterClient extends Component {
                 "Female"
             ],
             success: undefined,
-            loaded: false
         };
     }
 
     // Populate state data with data from the database for the pt
     static getDerivedStateFromProps(props, state) {
-        if (!props.authenticatedUser.user.pt) {
-            props.history.push('/users/' + props.authenticatedUser.user.id + '/dashboard');
-            return null
-        }
         if(!isEmpty(state.errors)){
             return {
                 errors: state.errors
@@ -46,7 +41,6 @@ class RegisterClient extends Component {
             return {
                 errors: props.errors,
                 message: props.success,
-                loaded: true
             }
         }
         return null
@@ -56,6 +50,7 @@ class RegisterClient extends Component {
         const {isAuthenticated} = this.props.authenticatedUser;
         if(!isAuthenticated)
             this.props.history.push('/login');
+        checkExp();
         this.props.clearErrors();
         this.props.clearSuccess();
         document.body.scrollTo(0,0);
@@ -138,90 +133,84 @@ class RegisterClient extends Component {
     };
 
     render() {
-        // if loaded is false then return loading screen
-        if (!this.state.loaded) {
+        if(!this.props.authenticatedUser.isAuthenticated){
             return <Loading myClassName="loading_container"/>
         }
-        if(isEmpty(this.props.authenticatedUser.user)){
-            return <ErrorComponent/>
-        }
-        else {
-            const {errors, message, FullName, Email, DateOfBirth, ContactNumber, Values} = this.state; // This allows errors to be pulled out of this.state with pulling them out directly
+        const {errors, message, FullName, Email, DateOfBirth, ContactNumber, Values} = this.state; // This allows errors to be pulled out of this.state with pulling them out directly
 
-            return (
-                <div className="register page-margin-top">
-                    <div className="container  register-custom">
-                        <div className="row">
-                            <div className="m-auto col-md-8">
-                                <p className="description text-center">Enter New Client Details Below</p>
-                                <form onSubmit={this.onSubmit}> {/* onSubmit used instead of normal action*/}
-                                    <FormInputGroup
-                                        myClassName="register-client"
-                                        name="FullName"
-                                        placeholder="Full Name"
-                                        value={FullName}
-                                        type="text"
-                                        onChange={this.onChange}
-                                        error={errors.FullName}
-                                    />
-                                    <FormInputGroup
-                                        myClassName="register-client"
-                                        name="Email"
-                                        placeholder="Email Address"
-                                        value={Email}
-                                        type="Email"
-                                        onChange={this.onChange}
-                                        error={errors.Email}
-                                    />
-                                    <div className="form-group edit-profile-date-div">
-                                        <div className="edit-date-div">
-                                            <label className="control-label form-control-lg edit-profile-label">Date of
-                                                Birth:
-                                            </label>
-                                            < FormInputGroup
-                                                myClassName="edit-exercise"
-                                                name="DateOfBirth"
-                                                value={DateOfBirth}
-                                                type="date"
-                                                onChange={this.onChange}
-                                                error={errors.DateOfBirth}
-                                            />
-                                        </div>
-                                        <div className="edit-gender-div">
-                                            <label
-                                                className="control-label form-control-lg edit-profile-label gender">
-                                                Gender:
-                                            </label>
-                                            <FormSelectComp
-                                                name="Sex"
-                                                id="Sex"
-                                                values={Values}
-                                                onChange={this.onChange}
-                                                error={errors.Sex}
-                                            />
-                                        </div>
+        return (
+            <div className="register page-margin-top">
+                <div className="container  register-custom">
+                    <div className="row">
+                        <div className="m-auto col-md-8">
+                            <p className="description text-center">Enter New Client Details Below</p>
+                            <form onSubmit={this.onSubmit}> {/* onSubmit used instead of normal action*/}
+                                <FormInputGroup
+                                    myClassName="register-client"
+                                    name="FullName"
+                                    placeholder="Full Name"
+                                    value={FullName}
+                                    type="text"
+                                    onChange={this.onChange}
+                                    error={errors.FullName}
+                                />
+                                <FormInputGroup
+                                    myClassName="register-client"
+                                    name="Email"
+                                    placeholder="Email Address"
+                                    value={Email}
+                                    type="Email"
+                                    onChange={this.onChange}
+                                    error={errors.Email}
+                                />
+                                <div className="form-group edit-profile-date-div">
+                                    <div className="edit-date-div">
+                                        <label className="control-label form-control-lg edit-profile-label">Date of
+                                            Birth:
+                                        </label>
+                                        < FormInputGroup
+                                            myClassName="edit-exercise"
+                                            name="DateOfBirth"
+                                            value={DateOfBirth}
+                                            type="date"
+                                            onChange={this.onChange}
+                                            error={errors.DateOfBirth}
+                                        />
                                     </div>
-                                    <FormInputGroup
-                                        myClassName="edit-contact-div"
-                                        name="ContactNumber"
-                                        placeholder="Enter Contact Number"
-                                        value={ContactNumber}
-                                        type="text"
-                                        onChange={this.onChange}
-                                        error={errors.ContactNumber}
-                                    />
-                                    <DisplayMessage message={message}/>
-                                    <input type="submit" value="Register Client" className="btn btn-info btn-block mt-1"/>
-                                    <button type="button" className="btn btn-success btn-block mt-3 mb-5"
-                                            onClick={this.props.history.goBack}>Back
-                                    </button>
-                                </form>
-                            </div>
+                                    <div className="edit-gender-div">
+                                        <label
+                                            className="control-label form-control-lg edit-profile-label gender">
+                                            Gender:
+                                        </label>
+                                        <FormSelectComp
+                                            name="Sex"
+                                            id="Sex"
+                                            values={Values}
+                                            onChange={this.onChange}
+                                            error={errors.Sex}
+                                        />
+                                    </div>
+                                </div>
+                                <FormInputGroup
+                                    myClassName="edit-contact-div"
+                                    name="ContactNumber"
+                                    placeholder="Enter Contact Number"
+                                    value={ContactNumber}
+                                    type="text"
+                                    onChange={this.onChange}
+                                    error={errors.ContactNumber}
+                                />
+                                <DisplayMessage message={message}/>
+                                <input type="submit" value="Register Client" className="btn btn-info btn-block mt-1"/>
+                                <button type="button" className="btn btn-success btn-block mt-3 mb-5"
+                                        onClick={this.props.history.goBack}>Back
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
-            );
-        }
+            </div>
+        );
     }
 }
 
@@ -244,4 +233,8 @@ const stateToProps = (state) => ({
 // connect must be exported with a passed parameter (not direct parameter) of Register this is wrapped with withRouter
 // allowing the functions of the package to be used with the component eg, proper routing, and direct parameters of
 // stateToProps for the 1st parameter and the action which is registerClient as the 2nd parameter
-export default connect(stateToProps, { registerClient, clearErrors, clearSuccess})(withRouter(RegisterClient));
+export default connect(stateToProps, {
+    registerClient,
+    clearErrors,
+    clearSuccess
+})(withRouter(RegisterClient));
