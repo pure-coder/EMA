6bbec73ac9eae5a9ef302c8f2797b4b7c6c1faf6
pure-coder <jrd259@my.open.ac.uk>
2019-09-12 18:35:49 +0100

    Error in creating the log dir in the wrong place, so changed fileName in createLogging from '../log/log.json' to './log/log.json'

--- app/scripts/createLogging.js
+++ app/scripts/createLogging.js
@@ -1,14 +1,14 @@
 let fs = require('fs');
 
-let dir = '../log';
-let fileName = '../log/log.json';
+let dir = './log';
+let fileName = './log/log.json';
 
 // Create log directory.
 fs.mkdir(dir, { recursive: true }, (err) => {
     if(err) throw err;
 });
 
-// Create log.json file for logging data
+// Create log.json file for logging data.
 fs.writeFile(fileName, '', (err) => {
     if(err) throw err;
     console.log("log file created!");
