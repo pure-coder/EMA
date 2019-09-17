ac0c3f3b49ece5e6a006122f446aa5e8f9f6e6ec
pure-coder <jrd259@my.open.ac.uk>
2019-09-17 01:44:42 +0100

    modified emailer.js env variables

--- app/services/emailer.js
+++ app/services/emailer.js
@@ -26,20 +26,20 @@ module.exports = function (Email, Token) {
         to: Email, // listed in rfc822 message header
         subject: 'Fitness App Activation code',
         envelope: {
-            from: `Fitness App <${EMAIL}>`, // used as MAIL FROM: address for SMTP
+            from: "Fitness App <" + EMAIL + ">", // used as MAIL FROM: address for SMTP
             to: Email + ', Mailer <' + Email + '>' // used as RCPT TO: address for SMTP
         },
         text: Token,
-        html: `Please click here on the <a href="${WEB_ADDRESS_ROOT}/api/verify?activation_link=` + Token +
-            '">Activation Link</a> for verification of this email address. Once clicked your account will be activated.'
+        html: "Please click here on the <a href=" + WEB_ADDRESS_ROOT + "/api/verify?activation_link=" + Token +
+            ">Activation Link</a> for verification of this email address. Once clicked your account will be activated."
     };
     let smtpTransport = nodemailer.createTransport(smtpConfig);
     smtpTransport.sendMail(message, function (error) {
         if (error) {
-          log.info(error)
+          log.info(error);
         }
         else {
-          log.info("Activation email sent ", Email)
+          log.info("Activation email sent to: ", Email);
         }
     });
 };
